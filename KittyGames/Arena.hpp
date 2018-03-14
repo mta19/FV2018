12fd9e36fd2ea4d6a112616f3353940ea7ab9452
mta19 <mta19@alu.ua.es>
2018-03-14 15:45:39 +0100

    Prueba Primeros Archivos
    Prueba para subir los primeros archivos.

---
+++ KittyGames/Arena.hpp
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Arena.hpp
+ * Author: manjaro
+ *
+ * Created on 14 de marzo de 2018, 15:37
+ */
+
+#ifndef ARENA_HPP
+#define ARENA_HPP
+
+
+
+#endif /* ARENA_HPP */
+
---
+++ KittyGames/Personaje.hpp
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Personaje.hpp
+ * Author: manjaro
+ *
+ * Created on 14 de marzo de 2018, 15:36
+ */
+
+#ifndef PERSONAJE_HPP
+#define PERSONAJE_HPP
+
+
+
+#endif /* PERSONAJE_HPP */
+
